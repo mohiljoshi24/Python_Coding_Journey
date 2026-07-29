240d4c3d6577a2191f4f4d905a8792bdaeeb408d
mohiljoshi24 <mohiljoshi245@outlook.com>
2026-07-29 22:59:38 +0530

    Type Casting & Workload Calculations
    Demonstrates converting user inputs (str) into numeric types (float / int) for mathematical operations.

---
+++ type_conversion.py
@@ -0,0 +1,7 @@
+max_workload = 5
+
+assigned_hours = float(input(" enter your assigned hour: "))
+
+remaining_hours = max_workload - assigned_hours
+
+print(f"remaining workload: {remaining_hours}")
