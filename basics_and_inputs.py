9faf028d0119d4b283ee4efdda3c58a3305fb7d3
mohiljoshi24 <mohiljoshi245@outlook.com>
2026-07-29 22:54:47 +0530

    Python Output, Dynamic Typing & User Inputs
    Demonstrates basic console printing, f-strings, and string inputs.

---
+++ basics_and_inputs.py
@@ -0,0 +1,8 @@
+age = 20
+print(f"your age is: {age}")
+
+username = input("enter your username: ")
+workload_hours = int(input("enter max workload hours: "))
+
+print(f"{username}")
+print(f"{workload_hours}")
